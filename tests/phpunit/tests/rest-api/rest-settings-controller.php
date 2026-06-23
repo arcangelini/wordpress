dbeb46ab39b95a54177d7141110b9ff580210369
Tony Arcangelini <tony@arcangelini.com>
2026-06-24 00:01:37 +0200

    Tests: Account for auto_approve_self_pingbacks in REST settings list

--- tests/phpunit/tests/rest-api/rest-settings-controller.php
+++ tests/phpunit/tests/rest-api/rest-settings-controller.php
@@ -117,6 +117,7 @@ class WP_Test_REST_Settings_Controller extends WP_Test_REST_Controller_Testcase 
 			'page_on_front',
 			'page_for_posts',
 			'default_ping_status',
+			'auto_approve_self_pingbacks',
 			'default_comment_status',
 			'site_icon', // Registered in wp-includes/blocks/site-logo.php
 		);
